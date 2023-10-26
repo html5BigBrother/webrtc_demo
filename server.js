ed7059552b2693a5d390519d12fc9e03b2b28171
严天宇5 <yantianyu5@hikvision.com.cn>
2023-10-26 13:41:59 +0800

    demo 100%

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const WebSocketServer = require('ws').Server,
-    wss = new WebSocketServer({port: 8888}),
+    wss = new WebSocketServer({ port: 8888 }),
     users = {};
 
 wss.on("connection", connection => {
@@ -9,37 +9,37 @@ wss.on("connection", connection => {
         // console.log("Got message:", message);
         let data, conn;
 
-        try{
+        try {
             data = JSON.parse(message);
-        }catch(e) {
+        } catch (e) {
             console.log(e);
             data = {};
         }
 
 
-        switch(data.type) {
+        switch (data.type) {
             case "login":
                 console.log("User logged in as", data.name);
-                if(users[data.name]) {
-                    sendTo(connection, {
-                        type: "login",
-                        success: false
-                    });
-                }else {
+                // if (users[data.name]) {
+                //     sendTo(connection, {
+                //         type: "login",
+                //         success: false
+                //     });
+                // } else {
                     users[data.name] = connection;
                     connection.name = data.name;
                     sendTo(connection, {
                         type: "login",
                         success: true
                     });
-                }
+                // }
                 break;
-            
+
             case "offer":
                 console.log("sending offer to:", data.name);
                 conn = users[data.name];
 
-                if(conn != null){
+                if (conn != null) {
                     connection.otherName = data.name;
                     sendTo(conn, {
                         type: "offer",
@@ -53,7 +53,7 @@ wss.on("connection", connection => {
                 console.log("sending answer to:", data.name);
                 conn = users[data.name];
 
-                if(conn != null){
+                if (conn != null) {
                     connection.otherName = data.name;
                     sendTo(conn, {
                         type: "answer",
@@ -66,26 +66,26 @@ wss.on("connection", connection => {
                 console.log("sending to", data.name);
                 conn = users[data.name];
 
-                if(conn != null){
+                if (conn != null) {
                     sendTo(conn, {
                         type: "candidate",
                         candidate: data.candidate
                     });
                 }
                 break;
-            
+
             case "leave":
                 console.log("Disconnected user from ", data.name);
                 conn = users[data.name];
                 conn.otherName = null;
 
-                if(conn != null){
+                if (conn != null) {
                     sendTo(conn, {
                         type: "leave"
                     });
                 }
                 break;
-                
+
             default:
                 sendTo(connection, {
                     type: "error",
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,75 @@
 
 <template>
   <header>
-
     <div class="wrapper">
-      <el-button @click="startLocal(1)">开启视频</el-button>
-      <el-button @click="startLocal(2)">开启麦克风</el-button>
-      <el-button @click="createPeer">创建peerConnection实例</el-button>
-      <el-button @click="addTrack">往实例添加流轨道</el-button>
-      <el-button @click="createOffer">createOffer创建本地媒体描述</el-button>
+      <el-row>
+        <span>对端流：</span>
+        <video id="remoteVideo"></video>
+      </el-row>
+      <el-row style="margin-top: 20px;">
+        <el-button @click="clickUser('user1')">通知websocket我的身份1</el-button>
+        <el-button @click="clickUser('user2')">通知websocket我的身份2</el-button>
+        <el-button @click="sendSDP">发送媒体描述</el-button>
+        <el-button @click="sendIce">发送ice信息</el-button>
+      </el-row>
+      <el-row style="margin-top: 20px;">
+        <el-button @click="startLocal(1)">开启视频</el-button>
+        <el-button @click="startLocal(2)">开启麦克风</el-button>
+        <el-button @click="startLocal(3)">开启屏幕</el-button>
+        <el-button @click="createPeer">创建peerConnection实例</el-button>
+        <el-button @click="addTrack">往实例添加流轨道</el-button>
+        <el-button @click="createOffer">createOffer创建本地媒体描述</el-button>
+      </el-row>
+      <el-row style="margin-top: 20px;">
+        <el-button @click="addRemoteSDP">添加对端媒体描述</el-button>
+        <el-button @click="addRemoteIce">添加对端ice</el-button>
+        <el-button @click="createAnswer">createAnswer应答对方媒体描述</el-button>
+      </el-row>
     </div>
   </header>
 
   <!-- <RouterView /> -->
 </template>
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 const ws = ref(null)
-const localStrem = ref(null)
+const userName = ref(null)
 const pcInstance = ref(null)
+const localStream = ref(null)
+const localIceList = ref(null)
+
+const remoteSDP = ref(null)
+const remoteIceList = ref(null)
+
+const remoteUser = computed(() => userName.value === 'user1' ? 'user2' : 'user1')
 
 onMounted(() => {
   ws.value = new WebSocket("ws://localhost:8888");
+  const iceList = []
+  ws.value.onmessage = (event) => {
+    const data = JSON.parse(event.data)
+    console.log('data', data)
+    if (data.type === 'offer') {
+      remoteSDP.value = data.offer
+    }
+    if (data.type === 'answer') {
+      remoteSDP.value = data.offer
+    }
+    if (data.type === 'candidate') {
+      iceList.push(data.candidate)
+      remoteIceList.value = iceList
+    }
+  }
 })
+// clickUser
+const clickUser = (user) => {
+  userName.value = user
+  ws.value.send(JSON.stringify({ type: 'login', name: user }))
+}
 // 获取本地流
 const startLocal = (type) => {
-  
+
   let options = {}
   if (type === 1) {
     options.video = true
@@ -33,17 +77,29 @@ const startLocal = (type) => {
   if (type === 2) {
     options.audio = true
   }
+  if (type === 3) {
+    navigator.mediaDevices.getDisplayMedia({ video: true }).then(stream => {
+      console.log(stream)
+      window.localStream = localStream.value = stream
+    })
+    return
+  }
   navigator.mediaDevices.getUserMedia(options).then(stream => {
     console.log(stream)
-    localStrem.value = stream
+    window.localStream = localStream.value = stream
   })
 }
-// createPeer
+// 创建pc实例
 const createPeer = () => {
   window.pcInstance = pcInstance.value = new RTCPeerConnection({ iceServers: [{ urls: 'stun:stun1.l.google.com:19302' }] })
   console.log(pcInstance.value)
+  const iceList = []
   pcInstance.value.onicecandidate = (event) => {
     console.log('onicecandidate', event)
+    if (event.candidate) {
+      iceList.push(event.candidate)
+      localIceList.value = iceList
+    }
   }
   pcInstance.value.oniceconnectionstatechange = (event) => {
     console.log('oniceconnectionstatechange', event)
@@ -51,11 +107,26 @@ const createPeer = () => {
   pcInstance.value.onicegatheringstatechange = (event) => {
     console.log('onicegatheringstatechange', event)
   };
+
+  pcInstance.value.ontrack = (event) => {
+    console.log('ontrack', event)
+    const remoteVideo = document.getElementById('remoteVideo')
+    remoteVideo.srcObject = event.streams[0]
+    remoteVideo.onloadedmetadata = function (e) {
+      remoteVideo.play();
+    };
+    event.track.onmute = function (ev) {
+      console.log("Remote track muted:", ev);
+    };
+    event.track.onunmute = function (ev) {
+      console.log("Remote track flowing again:", ev);
+    };
+  }
 }
 // 往连接注入流轨道
 const addTrack = () => {
-  for (const track of localStrem.value.getTracks()) {
-    pcInstance.value.addTrack(track, localStrem.value);
+  for (const track of localStream.value.getTracks()) {
+    pcInstance.value.addTrack(track, localStream.value);
   }
   console.log(pcInstance.value.getTransceivers())
 }
@@ -64,6 +135,36 @@ const createOffer = () => {
   pcInstance.value.createOffer().then(offer => {
     console.log(offer)
     pcInstance.value.setLocalDescription(offer)
+  })
+}
+// createAnswer
+const createAnswer = () => {
+  pcInstance.value.createAnswer().then(answer => {
+    console.log(answer)
+    pcInstance.value.setLocalDescription(answer)
+  })
+}
+
+// 发送媒体描述给对端
+const sendSDP = () => {
+  ws.value.send(JSON.stringify({ type: 'offer', name: remoteUser.value, offer: pcInstance.value.localDescription }))
+}
+// 发送ice到对端
+const sendIce = () => {
+  localIceList.value.forEach(candidate => {
+    ws.value.send(JSON.stringify({ type: 'candidate', name: remoteUser.value, candidate }))
+  })
+}
+
+
+// 添加对端sdp
+const addRemoteSDP = () => {
+  pcInstance.value.setRemoteDescription(remoteSDP.value)
+}
+// 添加对端ice
+const addRemoteIce = () => {
+  remoteIceList.value.forEach(candidate => {
+    pcInstance.value.addIceCandidate(candidate)
   })
 }
 
@@ -117,9 +218,6 @@ nav a:first-of-type {
   }
 
   header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
     width: 90vw;
   }
 
